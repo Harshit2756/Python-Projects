65d549e9d81161df0bef3a619fb22bc6982cc2d8
Harshit khandelwal <khandelwalharshit431@gmail.com>
2023-02-26 12:51:24 +0530

    taking input and speaking

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -1,8 +1,24 @@
-import pyttsx3
+import pyttsx3  # pip install pyttsx3  == text data into speach
 
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-print (voices)
+engine = pyttsx3.init()
 
 def speak(audio):
-    pass
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def getvoice():
+    voices = engine.getProperty('voices')
+    # print(voices[1].id)
+    if voices ==1:
+        engine.setProperty('voice', voices[0].id)
+    if voices == 2:
+        engine.setProperty('voice', voices[1].id)
+
+    speak =("this is jarvis sir")
+
+while True:
+    voices =int (input("Press 1 for male voice \n for female voice press 2 \n")) 
+    # speak(audio)
+    getvoice()
+    voices = engine.getProperty('voices')
