61ca421a5f9a11f4c64c7407720801ee992145fb
Harshit khandelwal <khandelwalharshit431@gmail.com>
2023-03-19 19:58:35 +0530

    phone number info

--- phone number/main.py
+++ phone number/main.py
@@ -12,19 +12,19 @@ while True:
 # CH is the country code passed to the parse function to get the country name
 # ch_number = phonenumbers.parse(number, "CH")
 # description_for_number is used to get the country name in english
-    Country_name=geocoder.description_for_number(number, "en")
-print("Country: "+ Country_name  )
+    Country_name = geocoder.description_for_number(number, "en")
+    print("Country: " + Country_name)
 
 # ********* carrier name *********
 # RO is the country code passed to the parse function to get the carrier name
 # service_number = phonenumbers.parse(number, "RO")
 # name_for_number is used to get the carrier name in english
-Carrier_Name=carrier.name_for_number(number, "en")
-print("Carrier: "+Carrier_Name)
+    Carrier_Name = carrier.name_for_number(number, "en")
+    print("Carrier: "+Carrier_Name)
 
 # ********* timezone *********
 # GB is the country code passed to the parse function to get the timezone
-gb_number = phonenumbers.parse(number, "GB")
+    gb_number = phonenumbers.parse(number, "GB")
 # time_zones_for_number is used to get the timezone
-Timezone = timezone.time_zones_for_number(gb_number)
-print("Timezone: "+ Timezone)
+    Timezone = timezone.time_zones_for_number(gb_number)
+    print("Timezone: " + Timezone)
