b1a0e12e26f2b8a5d39e9c2ab02fe60c2a5b2aba
Harshit khandelwal <khandelwalharshit431@gmail.com>
2023-03-20 13:27:53 +0530

    change the deflaut browser

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -1,25 +1,32 @@
 import pyttsx3  # pip install pyttsx3  == text data into speech
-import datetime # pip install datetime == get date and time
-import speech_recognition as sr  # pip install speechRecognition == speech data into text
-import smtplib # pip install secure-smtplib == send email
-from email.message import EmailMessage # pip install email == send email with attachment
-import wikipedia # pip install wikipedia == search wikipedia
-import webbrowser # pip install webbrowser == open browser
-from time import sleep # pip install time == sleep function to wait for some time
-import os # pip install os == open file
-import pyautogui # pip install pyautogui == take screenshot
+import datetime  # pip install datetime == get date and time
+# pip install speechRecognition == speech data into text
+import speech_recognition as sr
+import smtplib  # pip install secure-smtplib == send email
+# pip install email == send email with attachment
+from email.message import EmailMessage
+import wikipedia  # pip install wikipedia == search wikipedia
+import webbrowser  # pip install webbrowser == open browser
+from time import sleep  # pip install time == sleep function to wait for some time
+import os  # pip install os == open file
+import pyautogui  # pip install pyautogui == take screenshot
 # import psutil # pip install psutil == get battery percentage
 # import pyjokes # pip install pyjokes == get jokes
 
-from secrets import sender_email, sender_password, To # pip install python-dotenv == hide email and password
+# pip install python-dotenv == hide email and password
+from secrets import sender_email, sender_password, To
 engine = pyttsx3.init()
 
 # ********************* Speak Function ***********************
+
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
 # ********************* Voice Change Function ***********************
+
+
 def getvoices(voice):
     voices = engine.getProperty('voices')
     # print(voices[1].id)
@@ -31,12 +38,16 @@ def getvoices(voice):
         # speak("Hello sir to select my voice press 2")
 
 # ********************* Time Function ***********************
+
+
 def time():
     Time = datetime.datetime.now().strftime("%I:%M:%S")
     speak("THe current time is")
     speak(Time)
 
 # ********************* Date Function ***********************
+
+
 def date():
     year = int(datetime.datetime.now().year)
     month = int(datetime.datetime.now().month)
@@ -47,6 +58,8 @@ def date():
     speak(year)
 
 # ********************* Greeting Function ***********************
+
+
 def greeting():
     hour = datetime.datetime.now().hour
     if hour >= 6 and hour < 12:
@@ -59,22 +72,28 @@ def greeting():
         speak("Good Night")
 
 # ********************* Wishme Function ***********************
+
+
 def wishme():
     speak("Welcome back sir!")
     greeting()
     speak("Jarvis at your service. Please tell me how can i help you?")
 
 # ************ Take Command Through CMD Function *************
+
+
 def takeCommandCMD():
     query = input("Command: ")
     return query
 
 # ************ Take Command Through Mic Function *************
+
+
 def takeCommandMic():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1 # 1 second pause to indicate end of speaking
+        r.pause_threshold = 1  # 1 second pause to indicate end of speaking
         audio = r.listen(source)
 
     try:
@@ -89,14 +108,19 @@ def takeCommandMic():
         return "None"
     return query
 
-#*********************** Quit Function ***********************
+# *********************** Quit Function ***********************
+
+
 def quit():
     speak("Thanks for using me sir, have a good day.")
     exit()
 
 # ********************* Send Email Function ***********************
-def sendEmail(To, Subject , content):
-    server = smtplib.SMTP('smtp.gmail.com', 587) # 587 is port number for gmail server 
+
+
+def sendEmail(To, Subject, content):
+    # 587 is port number for gmail server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.login(sender_email, sender_password)
@@ -109,9 +133,12 @@ def sendEmail(To, Subject , content):
     server.close()
 
 # ********************* Send Whatsapp Message Function ***********************
+
+
 def sendwhatsappmsg(phone_no, message):
     Message = message
-    webbrowser.open("https://web.whatsapp.com/send?phone=+91" + phone_no + "&text=" + Message)
+    webbrowser.open("https://web.whatsapp.com/send?phone=+91" +
+                    phone_no + "&text=" + Message)
     sleep(10)
     pyautogui.press('enter')
 
@@ -120,6 +147,7 @@ def sendwhatsappmsg(phone_no, message):
 #     img = pyautogui.screenshot()
 #     img.save("C:\\Users\\Harsh\\Desktop\\Jarvis\\screenshot.png")
 
+
 # ********************* Main Function ***********************
 if __name__ == "__main__":
     getvoices(1)
@@ -127,9 +155,10 @@ if __name__ == "__main__":
 
     # ********** Logic for executing tasks based on query ***********
     while True:
-        
-        query = takeCommandMic().lower() # convert query into lower case so we don't get error in if condition when we say "Open Youtube" instead of "open youtube" or "OPEN YOUTUBE" etc.
-        
+
+        # convert query into lower case so we don't get error in if condition when we say "Open Youtube" instead of "open youtube" or "OPEN YOUTUBE" etc.
+        query = takeCommandMic().lower()
+
         # *********** time ***********
         if 'time' in query:
             time()
@@ -137,12 +166,14 @@ if __name__ == "__main__":
         # *********** date ***********
         elif 'date' in query:
             date()
-        
+
         # *********** wikipedia ***********
         elif 'wikipedia' in query:
             speak("Searching Wikipedia...")
-            query = query.replace("wikipedia", "")  # replace wikipedia with nothing in the query string so we don't get error in wikipedia search process when searching for something like "wikipedia time"
-            results = wikipedia.summary(query, sentences=2) # Wikipedia will give the summary in 2 sentences of the query  # pip install wikipedia
+            # replace wikipedia with nothing in the query string so we don't get error in wikipedia search process when searching for something like "wikipedia time"
+            query = query.replace("wikipedia", "")
+            # Wikipedia will give the summary in 2 sentences of the query  # pip install wikipedia
+            results = wikipedia.summary(query, sentences=2)
             speak("According to Wikipedia")
             print(results)
             speak(results)
@@ -150,37 +181,37 @@ if __name__ == "__main__":
         # *********** Open Youtube ***********
         elif 'open youtube' in query:
             speak("Opening Youtube...")
-            webbrowser.get('windows-default').open("youtube.com")
+            webbrowser.open("youtube.com")
 
         # *********** Open Google ***********
         elif 'open google' in query:
             speak("Opening Google...")
-            webbrowser.get('windows-default').open("google.com")
-        
+            webbrowser.open("google.com")
+
         # *********** Open Stackoverflow ***********
         elif 'open stackoverflow' in query:
             speak("Opening Stackoverflow...")
-            webbrowser.get('windows-default').open("stackoverflow.com")
-        
+            webbrowser.open("stackoverflow.com")
+
         # *********** Open Github ***********
         elif 'open github' in query:
             speak("Opening Github...")
-            webbrowser.get('windows-default').open("github.com")
+            webbrowser.open("github.com")
 
         # *********** Open Facebook ***********
         elif 'open facebook' in query:
             speak("Opening Facebook...")
-            webbrowser.get('windows-default').open("facebook.com")
-        
+            webbrowser.open("facebook.com")
+
         # *********** Open Instagram ***********
         elif 'open instagram' in query:
             speak("Opening Instagram")
-            webbrowser.get('windows-default').open("instagram.com")
+            webbrowser.open("instagram.com")
 
         # *********** Open Classroom ***********
         elif 'Classroom' in query:
             speak("opening Classroom")
-            webbrowser.get('windows-default').open("classroom.google.com")
+            webbrowser.open("classroom.google.com")
 
         # *********** Play music ***********
         elif 'play music' in query:
@@ -211,7 +242,7 @@ if __name__ == "__main__":
 
         # *********** Send Email ***********
         elif 'send email' in query:
-            email_list = {'testemail':''}
+            email_list = {'testemail': ''}
             try:
                 speak("To Whom you want to send email?")
                 To = email_list[takeCommandMic()]
@@ -227,10 +258,10 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 speak("Unable to send the email")
-        
+
         # *********** Send Whatsapp Message ***********
         elif 'send whatsapp message' in query:
-            user_name = {'test_name':'+91878855641'}
+            user_name = {'test_name': '+91878855641'}
             try:
                 speak("To whom you want to send whatsapp message?")
                 phone_no = user_name[takeCommandMic()]
@@ -254,5 +285,3 @@ if __name__ == "__main__":
         # *********** Exit ***********
         elif 'exit' in query:
             quit()
-            
-
